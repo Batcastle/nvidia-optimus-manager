377da007b89f81496e955fed1678d41ee2130dd8
Thomas Castleman <contact@draugeros.org>
2019-09-21 22:53:06 -0400

    Update nvidia-optimus-installer.sh
    * provided full path to desired functions to circumvent path injections and increase performance
    * prepended "builtin" where shell built-in functions where present. This will force the shell to use these built-ins
    * replaced mulitple lines of output with newline character (\n), so that it's all printed at once
       * make sure that echo is being passed at -e flag in these cases
     * replaced the DIR variable with the PWD global variable, as the pwd command simply echos this variable, thus better performance.

--- nvidia-optimus-installer.sh
+++ nvidia-optimus-installer.sh
@@ -9,91 +9,61 @@ GDM=/etc/gdm
 MOD=/etc/modprobe.d
 BIN=/usr/bin
 OPT=nvidia-optimus-manager
-DIR=`pwd`
 SYSD=nvidia-optimus-autoconfig.service
 ####################################
 
 # CHECK FOR PRIVS
 
-clear
-echo ""
-echo "     -------------------------------------------"
-echo ""
-echo "           NVIDIA-OPTIMUS-MANAGER INSTALLER"
-echo ""
-echo "     -------------------------------------------"
-echo ""
-sleep 3
-echo ""
-clear
-echo ""
-echo "This script will install and autoconfigure"
-echo "the Nvidia-Optimus-Manager for Solus Budgie"
-echo "or Gnome editions."
-echo ""
-echo "This WILL require a reboot, once completed"
-echo ""
-sleep 4
-clear
+/usr/bin/clear
+builtin echo -e "\n     -------------------------------------------\n\n           NVIDIA-OPTIMUS-MANAGER INSTALLER\n\n     -------------------------------------------\n"
+/bin/sleep 3
+builtin echo ""
+/usr/bin/clear
+builtin echo -e "\nThis script will install and autoconfigure\nthe Nvidia-Optimus-Manager for Solus Budgie\nor Gnome editions.\n\nThis WILL require a reboot, once completed.\n"
+/bin/sleep 4
+/usr/bin/clear
 
 # DESKTOP CHECKS & CONFIGURATION
 
-if [ `echo $XDG_CURRENT_DESKTOP` = "GNOME" ]
+if [ `builtin echo $XDG_CURRENT_DESKTOP` = "GNOME" ]
 then
-  echo ""
-  echo "Setting things up for Gnome..."
-  echo ""
-  sleep 2
-  sudo cp ${DIR}/99-nvidia.conf ${GDM}/99-nvidia.conf
-  sudo cp ${DIR}/${OPT} ${BIN}/${OPT}
-  sudo chmod a+x ${BIN}/${OPT}
-  sudo cp ${DIR}/${SYSD} /etc/systemd/system/${SYSD}
-  echo ""
-  sudo systemctl daemon-reload && sudo systemctl enable nvidia-optimus-autoconfig
-  echo ""
+  builtin echo -e "\nSetting things up for Gnome...\n"
+  /bin/sleep 2
+  /usr/bin/sudo /bin/cp ${PWD}/99-nvidia.conf ${GDM}/99-nvidia.conf
+  /usr/bin/sudo /bin/cp ${PWD}/${OPT} ${BIN}/${OPT}
+  /usr/bin/sudo /bin/chmod a+x ${BIN}/${OPT}
+  /usr/bin/sudo /bin/cp ${PWD}/${SYSD} /etc/systemd/system/${SYSD}
+  builtin echo ""
+  /usr/bin/sudo /bin/systemctl daemon-reload && /usr/bin/sudo /bin/systemctl enable nvidia-optimus-autoconfig
+  builtin echo ""
     # SYSTEM PREP
-    echo ""
-    echo "Getting things ready..."
-    echo ""
-    sudo mkdir -p ${MOD}
-    echo "blacklist nouveau" | sudo tee ${MOD}/blacklist-nouveau.conf
-    echo ""
-    echo "Installing necessary applications..."
-    echo ""
+    builtin echo -e "\nGetting things ready...\n"
+    /usr/bin/sudo /bin/mkdir -p ${MOD}
+    builtin echo "blacklist nouveau" | /usr/bin/sudo /usr/bin/tee ${MOD}/blacklist-nouveau.conf
+    builtin echo -e "\nInstalling necessary applications...\n"
 
-elif [ `echo $XDG_CURRENT_DESKTOP` = "Budgie:GNOME" ]
+elif [ `builtin echo $XDG_CURRENT_DESKTOP` = "Budgie:GNOME" ]
 then
-  echo ""
-  echo "Setting things up for Budgie..."
-  echo ""
-  sleep 2
-  sudo cp ${DIR}/99-nvidia.conf ${GDM}/99-nvidia.conf
-  sudo cp ${DIR}/${OPT} ${BIN}/${OPT}
-  sudo chmod a+x ${BIN}/${OPT}
-  sudo cp ${DIR}/${SYSD} /etc/systemd/system/${SYSD}
-  echo ""
-  sudo systemctl daemon-reload && sudo systemctl enable nvidia-optimus-autoconfig
-  echo ""
+  builtin echo -e "\nSetting things up for Budgie...\n"
+  /bin/sleep 2
+  /usr/bin/sudo /bin/cp ${PWD}/99-nvidia.conf ${GDM}/99-nvidia.conf
+  /usr/bin/sudo /bin/cp ${PWD}/${OPT} ${BIN}/${OPT}
+  /usr/bin/sudo /bin/chmod a+x ${BIN}/${OPT}
+  /usr/bin/sudo /bin/cp ${PWD}/${SYSD} /etc/systemd/system/${SYSD}
+  builtin echo ""
+  /usr/bin/sudo /bin/systemctl daemon-reload && /usr/bin/sudo /bin/systemctl enable nvidia-optimus-autoconfig
+  builtin echo ""
     # SYSTEM PREP
-    echo ""
-    echo "Getting things ready..."
-    echo ""
-    sudo mkdir -p ${MOD}
-    echo "blacklist nouveau" | sudo tee ${MOD}/blacklist-nouveau.conf
-    echo ""
-    echo "Installing necessary applications..."
-    echo ""
+    builtin echo -e "\nGetting things ready...\n"
+    /usr/bin/sudo /bin/mkdir -p ${MOD}
+    builtin echo "blacklist nouveau" | /usr/bin/sudo /usr/bin/tee ${MOD}/blacklist-nouveau.conf
+    builtin echo -e "\nInstalling necessary applications...\n"
 fi
-echo ""
-echo "               INSTALLATION COMPLETE!"
-echo ""
-echo "    You will need to reboot to apply all changes."
-echo ""
-echo ""
+builtin echo -e "\n               INSTALLATION COMPLETE!\n\n    You will need to reboot to apply all changes.\n\n"
 
-        read -p "              Reboot now (y/n)?" choice
+        builtin read -p "              Reboot now (y/n)?" choice
         case "$choice" in 
-          y|Y ) sudo reboot;;
-          n|N ) echo "Goodbye";;
+          y|Y ) /usr/bin/sudo /sbin/reboot;;
+          n|N ) builtin echo "Goodbye";;
         esac
         exit
